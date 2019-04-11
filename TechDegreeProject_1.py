bff01e9743e1a444f71b8f78ed33bcbed75a4b52
Jacob <samuel.blowe@yahoo.com>
2019-04-11 11:04:30 -0500

    Revert to previous version
    I let go of the high score list part of the exceeding expectations portion. I believe my code runs without errors. I have not made any changes since the review.

--- TechDegreeProject_1.py
+++ TechDegreeProject_1.py
@@ -2,22 +2,20 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 By Samuel Blowe
-I want to do the extra credit but I cannot seem to get the
-high score to work properply. Aside from that, this is my first draft!
+I want to do the extra credit but I decided to leave out the high score list sorting.
+I deleted all of the high score list parts from my code.
 --------------------------------
 
 """
 
 import random
 
-high_score = [] #list was ppreviously inside of while loop
 def guessing_game():
     
-    print("""Hello, player!
+    print("""Hello, player! 
         \nWelcome to the guessing game!
-        \nThe score is {} attempts!
         \nYou will be choosing a number between 1 and 10.
-        """.format(high_score))
+        """) #text header wecoming player to game.
     players_choice = 0
     solution = random.randint(1,10)
     player_attempt = 0
@@ -26,28 +24,27 @@ def guessing_game():
         players_choice = input("Choose a number:  ")
         player_attempt += 1
 
-        try:
+        try: # a try to catch errors while playing the game.
             players_choice = int(players_choice)
             if players_choice > 10:
                 raise ValueError("That value is invalid! Try again.")
             elif players_choice <= 0:
                 raise ValueError("That value is invalid! Try again.")
             elif players_choice < solution:
-                print("Higher. Try again.")
+                print("Higher. Try again.") #promt user for a guess and if answer is higher
             elif players_choice > solution:
-                print("Lower. Try again.")
+                print("Lower. Try again.") #prompt user for a guess and if answer is lower
             else:
                 print("You got the answer!")    
-        except ValueError as err:
+        except ValueError as err: # an except to report error to use in meaningful way
             print("Oh no! We ran into an issue. Please try again.")
 
-    print("You attempted {} times.".format(player_attempt))
-    high_score.extend([player_attempt])
+    print("You attempted {} times.".format(player_attempt)) #number of attempts to be displayed
 
     retry = input("Would you like another attempt at the game? (y/n):  ")
     if retry == str("y"):
         guessing_game()
     else:
-        print("Come back again to play again soon!\nGAME OVER")
+        print("Come back to play again soon!\nGAME OVER")
 
 guessing_game()
